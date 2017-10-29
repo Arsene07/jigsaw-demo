5fd5d70e29bfdc55b5dae62be92c68f9c2e9c081
Arsene07 <arsene07@hotmail.com>
2017-10-29 23:18:33 +0100

    sun.font compile

--- iki-team/build.sh
+++ iki-team/build.sh
@@ -1,14 +1,19 @@
 echo 'compiling module'
-javac -d classes --module-path ../mods --source-path src $(find . -name '*.java')
+#javac -d classes --module-path ../modules --source-path src $(find . -name '*.java')
+javac -d classes/ -cp ../libs/jackson-core-2.8.10.jar:../libs/jackson-annotations-2.8.10.jar --module-path ../modules/ --source-path src/ $(find . -name '*.java')
+
+
+#for sun.font:
+# javac -d classes --module-path ../modules  --class-path ../lib/jackson-core-2.8.10.jar  --add-exports java.desktop/sun.font=iki.team --source-path src $(find . -name '*.java')
 
 
 echo 'making module jar'
-jar --create  --file ../mods/iki-team.jar -C classes/ .
+jar --create  --file ../modules/iki-team.jar -C classes/ .
 
 
 echo 'Launching module jar'
-java --module-path ../mods/ -m iki.team/fr.zenika.iki.team.service.TeamManager
+java -cp ../libs/jackson-core-2.8.10.jar:../libs/jackson-annotations-2.8.10.jar --module-path ../modules/ -m iki.team/fr.zenika.iki.team.service.TeamManager
 
-jdeps --module-path ../mods/  ../mods/iki-team.jar
+#jdeps --module-path ../modules/  ../modules/iki-team.jar
 
-jdeps --module-path ../mods/ --jdk-internals  ../mods/iki-team.jar
+#jdeps --module-path ../modules/ --jdk-internals  ../modules/iki-team.jar
