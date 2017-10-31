b164f45c085178765c2a68671a83b91ae148dae6
Arsene07 <arsene07@hotmail.com>
2017-10-31 12:50:55 +0100

    iki-team fixes

--- iki-team/src/module-info.java
+++ iki-team/src/module-info.java
@@ -3,8 +3,6 @@ module iki.team{
     exports fr.zenika.iki.team.service;
     requires java.logging;
     requires java.sql;
-    /*requires jackson.core;
     requires jackson.databind;
-    requires jackson.annotations;
-    requires jdk.unsupported;*/
+    requires jdk.unsupported;
 }
