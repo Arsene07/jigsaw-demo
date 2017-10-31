372ae8797aabfb00f37e90c05c8ebc3105458981
Arsene07 <arsene07@hotmail.com>
2017-10-31 13:09:11 +0100

    fix paths

--- iki-api/build.sh
+++ iki-api/build.sh
@@ -1,10 +1,10 @@
 echo 'compiling module'
-javac -d classes --module-path ../mods --source-path src $(find . -name '*.java')
+javac -d classes --module-path ../modules --source-path src $(find . -name '*.java')
 
 
 echo 'making module jar'
-jar --create  --file ../mods/iki-api.jar -C classes/ .
+jar --create  --file ../modules/iki-api.jar -C classes/ .
 
 
 echo 'Launching module jar'
-java --module-path ../mods/ -m iki.api/fr.zenika.iki.api.controller.IkiAPI
+java --module-path ../modules/ --class-path ../libs/jackson-core-2.8.10.jar:../libs/jackson-annotations-2.8.10.jar -m iki.api/fr.zenika.iki.api.controller.IkiAPI
