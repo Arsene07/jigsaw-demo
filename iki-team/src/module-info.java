f5e696941b0391a1689c33e425d228d2de71d505
Arsene07 <arsene07@hotmail.com>
2017-10-31 12:58:44 +0100

    fixes for recruitment

--- iki-team/src/module-info.java
+++ iki-team/src/module-info.java
@@ -1,6 +1,7 @@
 module iki.team{
     exports fr.zenika.iki.team.domain;
     exports fr.zenika.iki.team.service;
+    opens fr.zenika.iki.team.domain;
     requires java.logging;
     requires java.sql;
     requires jackson.databind;
